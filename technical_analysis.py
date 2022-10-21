6f91f45c49d0287b55d9401e028178e7b3f6a956
bszek213 <bszekely@nevada.unr.edu>
2022-10-21 12:06:26 -0700

    FIX: handling of edge cases: insufficient funds, nans in lists

--- technical_analysis.py
+++ technical_analysis.py
@@ -10,7 +10,7 @@ limit to 5-10 trades a day
 from sklearn.linear_model import LinearRegression
 import krakenex
 from pykrakenapi import KrakenAPI
-from numpy import array, zeros, nan, arange, percentile, empty, log, median, mean, isnan
+from numpy import array, zeros, nan, arange, percentile, empty, log, mean, isnan,logical_not
 from time import sleep
 # import argparse
 from os import getcwd, remove
@@ -25,11 +25,17 @@ from buy_sell_signals import buy_signal_hft,basic_sell
 from tqdm import tqdm
 from glob import glob
 from psutil import virtual_memory
+import logging
 warnings.filterwarnings("ignore")
 """
-TODO: convert UTC to PST time
+TODO: 
+-convert UTC to PST time
+-change the sell condition to be the crossover points of the MACD or zero crossing of the Awe ind
 """
 SAMPLE_RATE = 5
+logging.basicConfig(filename=join(getcwd(),'errors.log'), level=logging.DEBUG, 
+                    format='%(asctime)s %(levelname)s %(name)s %(message)s')
+logger=logging.getLogger(__name__)
 class technical():
     def __init__(self):
         print('initialize kraken data')
@@ -138,16 +144,24 @@ class technical():
         self.data['RSI'] = 100 - (100/(1+self.data.RS))
         self.data['RSI'] = self.data['RSI'].rolling(5).mean()
         #calculate the percentile
-        self.q75, self.q25 = percentile(self.data['RSI'].dropna().values, [75 ,25])
-        #calculate an upper bound: Q3 + .2 *IQR
-        self.RSI_upper_bound = self.q75 + (.1*(self.q75-self.q25))
+        try:
+            self.q75, self.q25 = percentile(self.data['RSI'].dropna().values, [75 ,25])
+            #calculate an upper bound: Q3 + .2 *IQR
+            self.RSI_upper_bound = self.q75 + (.1*(self.q75-self.q25))
+        except:
+            print('IndexError: cannot do a non-empty take from an empty axes. default to 0')
+            self.q75, self.q25, self.RSI_upper_bound = 0,0,0
     def stoch_RSI(self):
         min_val  = self.data['RSI'].rolling(window=14, center=False).min()
         max_val = self.data['RSI'].rolling(window=14, center=False).max()
         self.data['Stoch_RSI'] = ((self.data['RSI'] - min_val) / (max_val - min_val)) * 100
         self.data['Stoch_RSI'] = self.data['Stoch_RSI'].rolling(9).mean()
-        self.q75_Stoch_RSI, self.q25_Stoch_RSI = percentile(self.data['Stoch_RSI'].dropna().values, 
-                                    [75 ,25])
+        try:
+            self.q75_Stoch_RSI, self.q25_Stoch_RSI = percentile(self.data['Stoch_RSI'].dropna().values, 
+                                                                [75 ,25])
+        except:
+            print('IndexError: cannot do a non-empty take from an empty axes. default to 0')
+            self.q75_Stoch_RSI, self.q25_Stoch_RSI = 0,0
     def moving_averages(self):
         self.data['ewmshort'] = self.data['close'].ewm(span=25, min_periods=25).mean() #used to be 50
         self.data['ewmmedium'] = self.data['close'].ewm(span=128, min_periods=128).mean()
@@ -166,6 +180,8 @@ class technical():
         self.volatility_value = self.data['log_return'].std()*len(self.data)**0.5 #365 days of trading square root
         self.coef_variation = self.data['log_return'].std() / self.data['log_return'].mean()
     def plot(self,name):
+        # xlim_val = [self.data.index[int((24*60)/SAMPLE_RATE)],self.data.index[-1]]
+        x_low_lim = self.data.index[-int((24*60)/SAMPLE_RATE)]
         fig, ax = plt.subplots(4,1,figsize=(12, 20)) 
         buy_df = self.data[~self.data['buy'].isnull()]
         sell_df = self.data[~self.data['sell'].isnull()]
@@ -189,6 +205,7 @@ class technical():
         #              ymax= max(self.data['close'].values),color='r',label='sell')
         ax[0].legend()
         ax[0].grid(True)
+        ax[0].set_xlim(left=x_low_lim)
         ax[0].set_xlabel('iterations')
         ax[0].set_ylabel('Close Price')
         #plot macd
@@ -206,6 +223,7 @@ class technical():
         ax[1].hlines(y = 0, xmin=self.data.index[0], xmax=self.data.index[-1])
         ax[1].legend()
         ax[1].grid(True)
+        ax[1].set_xlim(x_low_lim)
         ax[1].set_xlabel('iterations')
         ax[1].set_ylabel('MACD Values')
         #RSI
@@ -218,6 +236,7 @@ class technical():
         ax[2].scatter(buy_df.index, buy_df['RSI'], marker='o', s=120, color = 'g', label = 'buy')
         ax[2].scatter(sell_df.index, sell_df['RSI'], marker='o', s=120, color = 'r', label = 'sell')
         ax[2].legend()
+        ax[2].set_xlim(x_low_lim)
         ax[2].set_xlabel('iterations')
         ax[2].set_ylabel('stochRSI Values')
         ax[2].grid(True)
@@ -230,6 +249,7 @@ class technical():
         ax[3].fill_between(self.data.index, self.q75_ao, self.q25_ao, color='green',
                           alpha=0.2,label='IQR range AO')
         ax[3].legend()
+        ax[3].set_xlim(x_low_lim)
         ax[3].set_xlabel('iterations')
         ax[3].set_ylabel('Awesome Indicator')
         ax[3].grid(True)
@@ -283,48 +303,62 @@ class technical():
                         open_trade = False
             if (open_trade == False):
                 count_hold_iter +=1
-            if ((open_trade == False) and (thresh_buy < self.data['close'].iloc[o])):
+            #TODO change the sell condition to zero crossing of the Awe ind
+            if ((open_trade == False) and
+                (self.data['macd_diff'].iloc[o-1] > self.data['signal_line'].iloc[o-1]) and
+                (self.data['macd_diff'].iloc[o] < self.data['signal_line'].iloc[o]) and
+                (self.data['macd_diff'].iloc[o] > 0)
+                ):
                 self.data['sell'].iloc[o] = self.data['close'].iloc[o]
                 self.cumlative_gained += ((self.data['close'].iloc[o] - buy_price) / buy_price)*100
                 self.save_time_hold.append(count_hold_iter)
                 open_trade = True
-            if ((open_trade == False) and (thresh_sell > self.data['close'].iloc[o])):
-                self.data['sell'].iloc[o] = self.data['close'].iloc[o]
-                self.cumlative_gained += ((self.data['close'].iloc[o] - buy_price) / buy_price)*100
-                open_trade = True
+            # if (self.data['ao'].iloc[o] > 0):
+            # if ((open_trade == False) and (thresh_buy < self.data['close'].iloc[o])):         
+            # if ((open_trade == False) and (thresh_sell > self.data['close'].iloc[o])):
+            #     self.data['sell'].iloc[o] = self.data['close'].iloc[o]
+            #     self.cumlative_gained += ((self.data['close'].iloc[o] - buy_price) / buy_price)*100
+            #     open_trade = True
     def live_trading(self,name):
         trade_now = len(self.data) - self.buy_for_trading
         print(' ') #tqdm things
         print(f'closet trade was {trade_now} iterations ago')
         if trade_now <= 2:
-            print(f'buy {name}')
-            #put a buy function here : ad save the thresh and time? 
-            old_name = name.replace('USD','')
-            ind_cryp_t = self.crypto_list[self.crypto_list['crypto'] == old_name]
-            volume_inst = ind_cryp_t['Order'].values
-            balance = self.kraken.get_account_balance()
-            _, ask_price , _ = buy_signal_hft(name, self.kraken, volume_inst, balance.vol['ZUSD'])
-            threshold = ask_price + (ask_price * 0.0085)
-            thresh_sell = ask_price - (ask_price * (1.5*0.0085))
-            count_iter_hold = 0
-            sold_not_sold = 'not sold'
-            while True:
-                self.get_ohlc(name)
-                self.macd()
-                self.RSI()
-                # self.stoch_RSI()
-                self.aroon_ind()
-                self.moving_averages()
-                self.awesome_indicator()
-                self.half_LR()
-                self.volatility()
-                self.trade()
-                self.plot(name)
-                sold_not_sold = self.check_sell(name, volume_inst, ask_price, threshold, count_iter_hold,thresh_sell)
-                count_iter_hold+=1
-                if sold_not_sold == 'sold':
-                    break
-                sleep(SAMPLE_RATE*60)
+            try:
+                print(f'buy {name}')
+                #put a buy function here : ad save the thresh and time? 
+                old_name = name.replace('USD','')
+                ind_cryp_t = self.crypto_list[self.crypto_list['crypto'] == old_name]
+                volume_inst = ind_cryp_t['Order'].values
+                balance = self.kraken.get_account_balance()
+                money_min = float((self.kraken.get_ticker_information(name))['a'][0][0]) * volume_inst
+                if money_min < balance.vol['ZUSD']:
+                    _, ask_price , _ = buy_signal_hft(name, self.kraken, volume_inst, balance.vol['ZUSD'])
+                    threshold = ask_price + (ask_price * 0.0085)
+                    thresh_sell = ask_price - (ask_price * (1.5*0.0085))
+                    count_iter_hold = 0
+                    sold_not_sold = 'not sold'
+                    while True:
+                        self.get_ohlc(name)
+                        self.macd()
+                        self.RSI()
+                        # self.stoch_RSI()
+                        self.aroon_ind()
+                        self.moving_averages()
+                        self.awesome_indicator()
+                        self.half_LR()
+                        self.volatility()
+                        self.trade()
+                        self.plot(name)
+                        sold_not_sold = self.check_sell(name, volume_inst, ask_price, threshold, count_iter_hold,thresh_sell)
+                        count_iter_hold+=1
+                        if sold_not_sold == 'sold':
+                            break
+                        sleep(SAMPLE_RATE*60)
+                else:
+                    print(f'cannot buy {old_name}, not enough money in trading acoount')
+            except Exception as e:
+                logger.error(e)
     def check_sell(self,name,volume_inst,ask_price,threshold,count_iter_hold,thresh_sell):
         curr_ask = float((self.kraken.get_ticker_information(name))['a'][0][0])
         print(f'current ask: {curr_ask} : {threshold} threshold')
@@ -360,11 +394,12 @@ class technical():
                 self.half_LR()
                 self.volatility()
                 self.trade()
+                # if self.coef_variation != 'nan':
+                save_hist.append(self.coef_variation)
                 if ((self.lb_coef_deter < self.coef_variation) and 
                    (self.ub_coef_deter > self.coef_variation)):
                     self.plot(name)
                 self.live_trading(name)
-                save_hist.append(self.coef_variation)
                 save_hold_time_temp.append(self.save_time_hold)
                 print(f'cumulative gain {round(self.cumlative_gained,4)}% after running {name}')
                 sleep(1)
@@ -372,11 +407,13 @@ class technical():
             # plt.hist(save_hist,bins=100)
             # plt.show()
             # print(save_hold_time_temp)
-            coeff_var_75, coeff_var_25 = percentile(save_hist, 
+            save_hist_no_nan = [x for x in save_hist if str(x) != 'nan']
+            coeff_var_75, coeff_var_25 = percentile(save_hist_no_nan, 
                                         [75 ,25])
+                
             self.lb_coef_deter = coeff_var_25
             self.ub_coef_deter = coeff_var_75
-            print(f'LB {self.lb_coef_deter} : UP {self.ub_coef_deter}')
+            print(f'LB coef determination {self.lb_coef_deter} : UP coef determination {self.ub_coef_deter}')
             iter_hold_pos = [item for sublist in save_hold_time_temp for item in sublist]
             try:
                 iter_hold_pos_75,iter_hold_pos_25 = percentile(iter_hold_pos,[75 ,25])
