423da3bfd53c4ab0ada42125036a816e9a36110e
bszek213 <bszekely@nevada.unr.edu>
2022-10-26 22:30:46 -0700

    ADD: New data and strategy and data

--- technical_analysis.py
+++ technical_analysis.py
@@ -34,7 +34,7 @@ TODO:
 -convert UTC to PST time
 -change the sell condition to be the crossover points of the MACD or zero crossing of the Awe ind
 """
-SAMPLE_RATE = 30
+SAMPLE_RATE = 15
 logging.basicConfig(filename=join(getcwd(),'errors.log'), level=logging.DEBUG, 
                     format='%(asctime)s %(levelname)s %(name)s %(message)s')
 logger=logging.getLogger(__name__)
@@ -262,7 +262,8 @@ class technical():
     #     self.corr_ao_MACD = pearsonr(self.data['ao'].dropna().iloc[0:len_values].values,self.data['macd_diff'].dropna().iloc[0:len_values].values)
     def plot(self,name):
         #create new date time
-        self.data.index = date_range(end=datetime.now(),periods=len(self.data.index),freq="15min")
+        time_increment = SAMPLE_RATE + "min"
+        self.data.index = date_range(end=datetime.now(),periods=len(self.data.index),freq=time_increment)
         # xlim_val = [self.data.index[int((24*60)/SAMPLE_RATE)],self.data.index[-1]]
         x_low_lim = self.data.index[-int((24*60)/SAMPLE_RATE)]
         fig, ax = plt.subplots(6,1,figsize=(15, 20)) 
@@ -422,7 +423,7 @@ class technical():
                 (open_trade == False) and
                 (self.data['macd_diff'].iloc[o-1] > self.data['signal_line'].iloc[o-1]) and
                 (self.data['macd_diff'].iloc[o] < self.data['signal_line'].iloc[o]) and
-                (self.data['macd_diff'].iloc[o] > self.q75_macd)
+                (self.data['macd_diff'].iloc[o] >=0) #self.q75_macd
                 #I like this
                 # (open_trade == False) and
                 # (self.data['RSI'].iloc[o] > self.q75)
@@ -449,7 +450,7 @@ class technical():
     def live_trading(self,name):
         trade_now = len(self.data) - self.buy_for_trading
         print(' ') #tqdm things
-        print(f'closet trade was {trade_now} iterations ago')
+        print(f'{name} closet trade was {trade_now} iterations ago')
         if trade_now <= 1:
             try:
                 print(f'buy {name}')
@@ -459,7 +460,7 @@ class technical():
                 volume_inst = ind_cryp_t['Order'].values
                 balance = self.kraken.get_account_balance()
                 money_min = float((self.kraken.get_ticker_information(name))['a'][0][0]) * volume_inst
-                if money_min < balance.vol['ZUSD']:
+                if money_min  <=  balance.vol['ZUSD']:
                     _, ask_price , _ = buy_signal_hft(name, self.kraken, volume_inst, balance.vol['ZUSD'])
                     threshold = ask_price + (ask_price * 0.0085)
                     thresh_sell = ask_price - (ask_price * (1.5*0.0085))
@@ -488,13 +489,14 @@ class technical():
                 else:
                     print(f'cannot buy {old_name}, not enough money in trading acoount')
             except Exception as e:
+                print(e)
                 logger.error(e)
     def check_sell(self,name,volume_inst,ask_price,threshold,count_iter_hold,thresh_sell):
         curr_ask = float((self.kraken.get_ticker_information(name))['a'][0][0])
         print(f'current ask: {curr_ask} : {threshold} threshold')
         if ((self.data['macd_diff'].iloc[-2] > self.data['signal_line'].iloc[-2]) and
         (self.data['macd_diff'].iloc[-1] < self.data['signal_line'].iloc[-1]) and
-        (self.data['macd_diff'].iloc[-1] > self.q75_macd)
+        (self.data['macd_diff'].iloc[-1] >= 0)
         ):
             balance = self.kraken.get_account_balance()
             basic_sell(name, self.kraken, volume_inst, balance)
@@ -513,6 +515,8 @@ class technical():
         self.average_pos_hold = 76
         self.lb_coef_deter = 18.75
         self.ub_coef_deter = 60.35
+        closet_buy = 720
+        closet_name = '1Inch'
         while True:
             pos_crypt = self.get_24_above_zero()
             files = glob(join(getcwd(),'technical_analysis','*'))
@@ -537,14 +541,17 @@ class technical():
                 # self.correlational_analysis()
                 self.trade()
                 # if self.coef_variation != 'nan':
-                save_hist.append(self.coef_variation)
-                if (len(self.data) - self.buy_for_trading <(len(self.data)-1)): #int(len(self.data)/1.5)
+                check_latest_trade = len(self.data) - self.buy_for_trading
+                if (check_latest_trade < 700): #int(len(self.data)/1.5)
                     self.plot(name)
                 self.live_trading(name)
-                save_hold_time_temp.append(self.save_time_hold)
-                # print(self.corr_RSI_MACD)
-                # print(self.corr_ao_MACD)
+                if check_latest_trade < closet_buy:
+                    closet_buy = check_latest_trade
+                    closet_name = name
                 print(f'cumulative gain {round(self.cumlative_gained,4)}% after running {name}')
+                print(f'{closet_name} has the closest trade at {closet_buy} iterations')
+                save_hold_time_temp.append(self.save_time_hold)
+                save_hist.append(self.coef_variation)
                 sleep(1)
             # plt.figure()
             # plt.hist(save_hist,bins=100)
