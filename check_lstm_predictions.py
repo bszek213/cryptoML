e57eaf0aff0c5230ed0be4b24491622b94f48d40
bszek213 <bszekely@nevada.unr.edu>
2022-11-21 18:48:32 -0800

    ADD: messing with check_lstm plots

--- check_lstm_predictions.py
+++ check_lstm_predictions.py
@@ -3,6 +3,7 @@ import yfinance as yf
 import matplotlib.pyplot as plt
 import sys
 import os
+from datetime import datetime
 csv_name = f'{sys.argv[1]}_future_lstm.csv'
 final_dir = os.path.join(os.getcwd(), 'lstm_data', csv_name)
 btc_price = pd.read_csv(final_dir)
@@ -23,6 +24,13 @@ for i in range(len(fore_pct_change)):
     else:
         save_predicted_price.append(save_predicted_price[i-1] + (save_predicted_price[i-1] * fore_pct_change[i]))
 plt.plot(temp_df.Date,save_predicted_price,color='r',label='predicted_data')
+num_get = int(len(data)/2)
+start = btc_price.Date.iloc[num_get]
+end = temp_df.Date.iloc[-1]
+#temp_val = datetime.strptime(temp_val, '%Y-%m-%d')
+plt.gca().set_xlim(left=start,right=end)
+plt.gca().set_ylim([min(data.Close.iloc[-num_get:]), max(save_predicted_price)]) #max(data.Close.iloc[-num_get:])
+#plt.xlim(left=temp_df.Date.iloc[num_get])
 plt.legend()
 plt.title(sys.argv[1])
 plt.show()
